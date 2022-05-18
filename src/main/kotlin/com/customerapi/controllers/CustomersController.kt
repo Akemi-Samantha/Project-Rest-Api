93dad5860bb4919e7e362196fe68077cbb9aefc8
Akemi Samantha <samantha.nakayama@gmail.com>
2022-05-18 13:28:30 -0300

    bla

--- src/main/kotlin/com/customerapi/controllers/CustomersController.kt
+++ src/main/kotlin/com/customerapi/controllers/CustomersController.kt
@@ -1,24 +1,18 @@
 package com.customerapi.controllers
 
-import com.customerapi.controllers.dto.request.PostAddressRequest
 import com.customerapi.controllers.dto.request.PostCustomerRequest
 import com.customerapi.controllers.dto.request.PutCustomerRequest
 import com.customerapi.extension.toCustomerModel
 import com.customerapi.model.CustomerModel
-import com.customerapi.repository.RepositoryTest
-import com.customerapi.service.AddressService
 import com.customerapi.service.CustomerService
 import org.springframework.http.HttpStatus
-import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
 
 @RestController
 @RequestMapping("customers")
 
 class CustomersController(
-    var customerService: CustomerService,
-    val repositoryTest: RepositoryTest,
-    var addressController: AddressController
+    var customerService: CustomerService
 ) {
 
     @GetMapping
